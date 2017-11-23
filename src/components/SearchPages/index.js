f67083aea50526e22f9ccee5f021d7a41d3b18f4
speveri <silvia@smallfish.com>
2017-11-23 17:18:10 +0100

    associated name of page selected

--- src/components/SearchPages/index.js
+++ src/components/SearchPages/index.js
@@ -24,9 +24,15 @@ class SearchPages extends Component {
     this.props.listingStore.history.push(window.location.pathname + "?" + newSearch);
     this.props.listingStore.filters.selectedPages.splice(pagesFromQueryString.findIndex(p => p === pageId), 1);
   };
+  getName = pageId => {
+    if (this.props.listingStore.pages) {
+      return this.props.listingStore.pages.find(page => page.objectId === pageId).name;
+    }
+  };
 
   render() {
     if (this.props.listingStore.pages) {
+      this.pagesObj = this.props.listingStore.pages;
       this.pages = this.props.listingStore.pages.filter(p => p !== undefined).map(page => {
         return (
           <option key={page.objectId} value={page.objectId} id={page.name}>
@@ -36,21 +42,25 @@ class SearchPages extends Component {
       });
     }
     const selectedPages = this.props.listingStore.filters.selectedPages.map(page => {
+      const pageName = this.getName(page);
+
       return (
-        <Flex className="tag" key={page}>
+        <Flex className="tag" key={page} justify="center">
           {" "}
-          {page} <i className="fa fa-times" aria-hidden="true" onClick={() => this.removeFilter(page)} />
+          {pageName} <i className="fa fa-times" aria-hidden="true" onClick={() => this.removeFilter(page)} />
         </Flex>
       );
     });
 
     return (
-      <Flex className="SearchPages">
-        <Flex style={{ marginRight: "15px" }}>Select a page</Flex>
-        <select onChange={this.handleChange}>
-          <option value="null">None</option>
-          {this.pages}
-        </select>
+      <Flex className="SearchPages" column align="start">
+        <Flex>
+          <Flex style={{ marginRight: "15px" }}>Select a page</Flex>
+          <select onChange={this.handleChange}>
+            <option value="null">None</option>
+            {this.pages}
+          </select>
+        </Flex>
         {this.props.listingStore.filters.selectedPages.length > 0 && (
           <Flex align="start" justify="center" className="tags">
             {selectedPages}{" "}
