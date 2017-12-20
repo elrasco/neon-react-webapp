ae67b5e2abff8d4bc54acfc99c19e8f303231204
speveri <silvia@smallfish.com>
2017-12-20 17:43:20 +0100

    new route for hairstyles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Listing from "./components/Listing";
 import Detail from "./components/Detail";
+import Hairstyles from "./components/Hairstyles";
 import Settings from "./components/Settings";
 import { Provider } from "mobx-react";
 import listingStore from "./stores/ListingStore";
@@ -24,6 +25,7 @@ class App extends Component {
             </Switch>
             <Route exact={true} path={"/listing/:type/:period/:sort/:w"} search="?pages=:data" component={Listing} />
             <Route exact={true} path={"/detail/:type/:objectId"} component={Detail} />
+            <Route exact={true} path={"/hairstyles"} component={Hairstyles} />
             <Route exact={true} path={"/settings"} component={Settings} />
           </div>
         </Router>
---
+++ src/components/Hairstyles/index.js
@@ -0,0 +1,339 @@
+import React, { Component } from "react";
+import { Flex } from "reflexbox";
+import Header from "../Header";
+import "./index.css";
+
+class Hairstyles extends Component {
+  render() {
+    return (
+      <Flex column>
+        <Header />
+        <Flex column className="hacks-section">
+          <h1>Hacks, Hacks & Hacks!</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              title="1"
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPOPxoBeauty%2Fvideos%2F1562894810425245%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              title="2"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fgorgeous.side%2Fvideos%2F1097534010389183%2F&show_text=1&width=476"
+              width="476"
+              height="587"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fgorgeous.side%2Fvideos%2F1698347443520458%2F&show_text=1&width=476"
+              width="476"
+              height="625"
+              title="3"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Curling Hacks</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              title="4"
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcosmopolitanuk%2Fvideos%2F813424652152491%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Tips & Tutorials</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPOPxoWedding%2Fvideos%2F1988835218013380%2F&show_text=1&width=560"
+              width="560"
+              height="426"
+              title="5"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialhairdiaries%2Fvideos%2F424656287884339%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="6"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FMetDaanMagazine%2Fvideos%2F2212374185666960%2F&show_text=1&width=476"
+              width="476"
+              height="631"
+              title="7"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialhairtutorials%2Fvideos%2F826718017531494%2F&show_text=1&width=476"
+              width="476"
+              height="587"
+              title="8"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialhairr%2Fvideos%2F1483544685044547%2F&show_text=1&width=476"
+              width="476"
+              height="543"
+              title="9"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <div className="placeholder" style={{ width: "350px" }} />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>VCS (Editorial, Curiosità, Persone straordinarie, Hairstyle Incredibili…)</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FAlyssaMilano%2Fvideos%2F908244855997963%2F&show_text=1&width=476"
+              width="476"
+              height="593"
+              title="10"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyahoonews%2Fvideos%2F936820463140402%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="11"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FCosmopolitan%2Fvideos%2F10155645613537708%2F&show_text=1&width=476"
+              width="476"
+              height="606"
+              title="12"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Incredible Hairstyles</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fveriapriyatno%2Fvideos%2F10155892527504903%2F&show_text=1&width=476"
+              width="476"
+              height="612"
+              title="13"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Dolls Hairstyle</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FMetDaanBeauty%2Fvideos%2F730798497122480%2F&show_text=1&width=476"
+              width="476"
+              height="587"
+              title="14"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Hair Transformation (professional)</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fjustgirlyth%2Fvideos%2F2081638278722557%2F&show_text=1&width=476"
+              width="476"
+              height="587"
+              title="15"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialhairr%2Fvideos%2F1566565910075757%2F&show_text=1&width=380"
+              width="380"
+              height="615"
+              title="16"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialhairdiaries%2Fvideos%2F444893542527280%2F&show_text=1&width=380"
+              width="380"
+              height="652"
+              title="17"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fthisisinsider%2Fvideos%2F1739947286312662%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="18"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+        <Flex column className="hacks-section">
+          <h1>Beard</h1>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbuzzfeednifty%2Fvideos%2F1966336500287739%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="19"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPopSugarMoms%2Fvideos%2F10155871480304863%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="20"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fstylemybodyapp%2Fvideos%2F681239455417642%2F&show_text=1&width=380"
+              width="380"
+              height="708"
+              title="21"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FInsiderbeauty%2Fvideos%2F517492198621183%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="22"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frayhigdonpage%2Fvideos%2F1862716520408733%2F&show_text=1&width=476"
+              width="476"
+              height="593"
+              title="23"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLADbible%2Fvideos%2F4199462683434223%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="24"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+          <Flex className="iframe-container" justify="space-between">
+            <iframe
+              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fsuperdeluxevideo%2Fvideos%2F555987308079157%2F&show_text=1&width=476"
+              width="476"
+              height="568"
+              title="25"
+              style={{ border: "none", overflow: "hidden" }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            />
+          </Flex>
+        </Flex>
+      </Flex>
+    );
+  }
+}
+export default Hairstyles;
